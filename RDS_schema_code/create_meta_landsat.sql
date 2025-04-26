8f847462b712388fe16f817d2f5fb9c0326d9957
Weihang Zhang <wz151@duke.edu>
2025-04-26 16:32:51 -0400

    add s3 link:

--- RDS_schema_code/create_meta_landsat.sql
+++ RDS_schema_code/create_meta_landsat.sql
@@ -1,14 +1,13 @@
-
-
-CREATE TABLE if not exists landsat_scenes (
+CREATE TABLE IF NOT EXISTS landsat_scenes (
     id SERIAL PRIMARY KEY,
-    scene_id TEXT UNIQUE NOT NULL,       -- 原始场景ID，全字符串
-    satellite TEXT NOT NULL,             -- LC08
-    processing_level TEXT NOT NULL,      -- L2SR
-    wrs_path TEXT NOT NULL,              -- 前3位：200
-    wrs_row TEXT NOT NULL,               -- 后3位：115
-    acquisition_date DATE NOT NULL,      -- 2016-11-07
-    processing_version TEXT NOT NULL,    -- 02
-    data_category TEXT NOT NULL,         -- T1 or T2
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    scene_id TEXT UNIQUE NOT NULL,
+    satellite TEXT NOT NULL,
+    processing_level TEXT NOT NULL,
+    wrs_path TEXT NOT NULL,
+    wrs_row TEXT NOT NULL,
+    acquisition_date DATE NOT NULL,
+    processing_version TEXT NOT NULL,
+    data_category TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    s3link TEXT
 );
